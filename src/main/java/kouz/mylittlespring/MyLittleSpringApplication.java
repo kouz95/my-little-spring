33b6b014fed61e5eee3ddf09c8067f5e54fcac67
김경준 <kouz@woowahan.com>
2021-08-14 16:26:56 +0900

    chore: init

---
+++ src/main/java/kouz/mylittlespring/MyLittleSpringApplication.java
@@ -0,0 +1,13 @@
+package kouz.mylittlespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLittleSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kouz/mylittlespring/MyLittleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kouz.mylittlespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLittleSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
